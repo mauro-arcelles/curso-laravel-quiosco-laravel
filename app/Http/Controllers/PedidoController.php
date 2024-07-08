4771c19a4f0914f5ebb95dc43d32afa5532869da
mauro-arcelles <mauroar1001@hotmail.com>
2024-07-07 20:08:26 -0500

    feat: Add PedidoController for managing orders and products in orders

---
+++ app/Http/Controllers/PedidoController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pedido;
+use App\Models\PedidoProducto;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PedidoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $pedido = new Pedido;
+        $pedido->user_id = Auth::user()->id;
+        $pedido->total = $request->total;
+        $pedido->save();
+
+        $id = $pedido->id;
+
+        $productos = $request->productos;
+
+        $pedido_producto = [];
+
+        foreach ($productos as $producto) {
+            $pedido_producto[] = [
+                'pedido_id' => $id,
+                'producto_id' => $producto['id'],
+                'cantidad' => $producto['cantidad'],
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ];
+        }
+
+        PedidoProducto::insert($pedido_producto);
+
+        return [
+            'message' => 'Pedido realizado correctamente. Estará listo en unos minutos'
+        ];
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Pedido $pedido)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Pedido $pedido)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Pedido $pedido)
+    {
+        //
+    }
+}
